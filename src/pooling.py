c8846030dc51ccf1424478413c697f2c5b47cf09
sdkaraban <jupyter-semyon@instance-senatorov-a100.asia-northeast3-a.c.smtb-labs.internal>
2024-08-03 19:57:17 +0000

    removed hidden dim from global pooling

--- src/pooling.py
+++ src/pooling.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 
 class GlobalAttentionPooling(nn.Module):
-    def __init__(self, input_dim, hidden_dim=None):  # No need for hidden_dim for global pooling
+    def __init__(self, input_dim):
         super(GlobalAttentionPooling, self).__init__()
 
         self.linear_key = nn.Linear(input_dim, 1)  # Project to a single dimension
