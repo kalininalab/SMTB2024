1e13c311df0bde596210dc847093b7277ef85c60
Ilya Senatorov <il.senatorov@protonmail.com>
2024-08-03 22:40:14 +0200

    working version

--- pretrain.py
+++ pretrain.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+from pathlib import Path
 
 from datasets import load_dataset
 from transformers import DataCollatorForLanguageModeling, EsmConfig, EsmForMaskedLM, Trainer, TrainingArguments
@@ -10,22 +11,14 @@ os.environ["TOKENIZERS_PARALLELISM"] = "true"
 
 parser = argparse.ArgumentParser(description="Pretrain a model")
 parser.add_argument("--data", type=str, default="khairi/uniprot-swissprot", help="Name of data to be trained on")
-parser.add_argument("--tokenizer", default="char", type=str, choices=["char", "bpe"], help="Tokenizer to use")
+parser.add_argument("--tokenizer", default="bpe", type=str, help="Tokenizer to use")
 parser.add_argument("--vocab_size", default=5000, type=int, help="Vocabulary size")
 parser.add_argument("--n_layers", type=int, default=12, help="Number of layers in the model")
 parser.add_argument("--n_dims", type=int, default=480, help="Dimensions of the model")
 parser.add_argument("--n_heads", type=int, default=16, help="Number of heads in the model")
 parser.add_argument("--epochs", type=int, default=100, help="Number of epochs to train")
-parser.add_argument("--batch_size", type=int, default=64, help="Batch size")
-parser.add_argument(
-    "--output_dir", type=str, default="/scratch/output", help="Output directory for model and checkpoints"
-)
-parser.add_argument(
-    "--token_output_file",
-    type=str,
-    default="/scratch/output/tokenizer/tokenizer.json",
-    help="Where the tokenizer json file will be saved",
-)
+parser.add_argument("--batch_size", type=int, default=4, help="Batch size")
+parser.add_argument("--output_dir", type=Path, default="data", help="Output directory for model and checkpoints")
 config = parser.parse_args()
 
 ## Load the dataset ##
@@ -34,10 +27,10 @@ dataset = load_dataset(config.data)
 ### Train the tokenizer & Load the pretrained tokenizer ###
 # You can choose the tokenizer type, default is bpe
 tokenizer = train_tokenizer(
-    dataset=dataset,
+    data=dataset["train"]["Sequence"],
     tokenization_type=config.tokenizer,
     vocab_size=config.vocab_size,
-    output_file_directory=config.token_output_file,
+    output_dir=config.output_dir,
 )
 
 # Add padding token to the tokenizer if not already present
@@ -47,7 +40,7 @@ if tokenizer.pad_token is None:
 
 ### Tokenize the dataset
 def tokenize_function(examples: dict) -> dict:
-    max_length = 1024  # Define the maximum length for the sequences
+    max_length = 255  # Define the maximum length for the sequences
     tokens = tokenizer(examples["Sequence"], padding="max_length", truncation=True, max_length=max_length)
     return tokens
 
@@ -61,6 +54,7 @@ esm_config = EsmConfig(
     hidden_size=config.n_dims,
     num_attention_heads=config.n_heads,
     pad_token_id=tokenizer.pad_token_id,
+    max_position_embeddings=256,
 )
 
 model = EsmForMaskedLM(
@@ -74,6 +68,7 @@ training_args = TrainingArguments(
     output_dir=config.output_dir,
     num_train_epochs=config.epochs,
     per_device_train_batch_size=config.batch_size,
+    use_cpu=True,
 )
 
 trainer = Trainer(
