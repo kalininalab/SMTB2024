9e45a899ba178d130729ce8cd1e1365108d38580
Old-Shatterhand <joeres.roman@web.de>
2024-08-04 13:15:45 +0000

    Extract embeddings from ESM

--- src/data.py
+++ src/data.py
@@ -1,4 +1,7 @@
+import os
+import pickle
 from typing import Any, Literal
+from pathlib import Path
 
 import esm
 import pandas as pd
@@ -25,19 +28,16 @@ class DownstreamDataset(Dataset):
 
 
 class ESMEmbedder:
-    def __init__(self, num_layers: Literal[6, 12, 30, 33]):
+    def __init__(self, num_layers: Literal[6, 12, 30, 33, 36, 48]):
         self.num_layers = num_layers
         self.models = {
-            6: "esm2_t6_8M_UR50D",
-            12: "esm2_t12_35M_UR50D",
-            30: "esm2_t30_150M_UR50D",
+            48: "esm2_t48_15B_UR50D",
+            36: "esm2_t36_3B_UR50D",
             33: "esm2_t33_650M_UR50D",
+            30: "esm2_t30_150M_UR50D",
+            12: "esm2_t12_35M_UR50D",
+            6: "esm2_t6_8M_UR50D",
         }
-
-        if self.num_layers not in self.models:
-            raise ValueError(
-                f"Unsupported number of layers: {self.num_layers}. Supported layers are: {list(self.models.keys())}"
-            )
 
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.model, self.alphabet = getattr(esm.pretrained, self.models[self.num_layers])()
@@ -46,6 +46,7 @@ class ESMEmbedder:
 
     def run(
         self,
+        dataset: str,
         data: list[str],
         layers: list[int] | None = None,
         contacts: bool = False,
@@ -59,35 +60,82 @@ class ESMEmbedder:
         """
         if layers is None:
             layers = range(self.num_layers + 1)
-        results = []
-        for prot in tqdm(data):
+        for i, prot in tqdm(enumerate(data)):
             batch_labels, batch_strs, batch_tokens = self.batch_converter([("alper", prot)])
             batch_tokens = batch_tokens.to(self.device)  # Ensure tokens are on the GPU
             with torch.no_grad():
                 i = self.model.forward(batch_tokens, repr_layers=layers, return_contacts=contacts)
-                detached_i = {}
-                for k, v in i.items():
-                    if isinstance(v, dict):  # Check if value is a dictionary (like "representations")
-                        detached_i[k] = {k1: v1.detach().cpu() for k1, v1 in v.items()}
-                    else:
-                        detached_i[k] = v.detach().cpu()
-                results.append(detached_i)
-        return results
+            for k, v in i.items():
+                if isinstance(v, dict):  # Check if value is a dictionary (like "representations")
+                    embedding = {k1: v1.detach().cpu() for k1, v1 in v.items()}
+                else:
+                    embedding = v.detach().cpu()
+            
 
 
-class DataRead:
-    def get_protlist(df):
-        data = pd.read_csv(df)
-        prot_list = []
-        d_dict = data.to_dict(orient="index")
-        for key in d_dict.keys():
-            n = d_dict[key][list(d_dict[key].keys())[0]]
-            prot_list.append(n)
-        return prot_list
+def get_protlist(df: str):
+    """
+    Get the protein sequences from the given DataFrame.
 
-    def embeddings_to_dataset(dataframe, embeddings, layer):
-        labels = list(dataframe[dataframe.columns[1]])
-        embedd_list = []
-        for i in range(len(embeddings)):
-            embedd_list.append(embeddings[i]["representations"][layer])
-        return DownstreamDataset(embedd_list, labels)
+    :param df: Path to the DataFrame
+    :return: A list of protein sequences
+    """
+    # print(df)
+    data = pd.read_csv(df)
+    prot_list = []
+    d_dict = data.to_dict(orient="index")
+    for key in d_dict.keys():
+        n = d_dict[key][list(d_dict[key].keys())[0]]
+        prot_list.append(n)
+    return prot_list
+
+
+def embeddings_to_dataset(dataframe: pd.DataFrame, embeddings: list[dict[Any, dict[Any, Any] | Any]], layer: int):
+    """
+    Convert the embeddings to a DownstreamDataset object.
+
+    :param dataframe: DataFrame of the dataset
+    :param embeddings: List of embeddings for each protein sequence
+    :param layer: Layer of the model to use for the embeddings
+    :return: DownstreamDataset object
+    """
+    labels = list(dataframe[dataframe.columns[1]])
+    embedd_list = []
+    for i in range(len(embeddings)):
+        embedd_list.append(embeddings[i]["representations"][layer].mean([0, 1]))
+    return DownstreamDataset(embedd_list, labels)
+
+
+model_names = {
+    48: "esm2_t48_15B_UR50D",
+    36: "esm2_t36_3B_UR50D",
+    33: "esm2_t33_650M_UR50D",
+    30: "esm2_t30_150M_UR50D",
+    12: "esm2_t12_35M_UR50D",
+    6: "esm2_t6_8M_UR50D",
+}
+
+
+def load_dataset(nlayers: int, dataset_name: str):
+    """
+    Preprocess the dataset from the given path and compute the embeddings for the model with nlayers-many layers.
+
+    :param path: Path to the dataset
+    :param nlayers: Maximum number of layers of the model to use for embedding
+    :return: A list of DownstreamDataset objects, one for each layer of the model
+    """
+    root = Path("/") / "scratch" / "data_roman" / dataset_name
+    model_name = model_names[nlayers]
+    for split in ["train", "val", "test"]:
+        print("embedding", split)
+        output_path = root / "processed" / model_name / split
+        # output_path.mkdir(exist_ok=True, parents=True)
+        print(root / "raw" / f"{split}.csv")
+        protlist = get_protlist(root / "raw" / f"{split}.csv")
+        fasta_path = root / f"{split}.fasta"
+        with open(fasta_path, "w") as f:
+            for i, seq in enumerate(protlist):
+                print(f">Prot{i:06d}", seq, sep="\n", file=f)
+        print(f"COMMAND: python -m src.extract {model_name} {output_path} {str(output_path)} --include per_tok")
+        os.system(f"python -m src.extract {model_name} {str(fasta_path)} {str(output_path)} --include per_tok")
+        
--- train.py
+++ train.py
@@ -1,40 +1,52 @@
-import torch
-import wandb
-from pytorch_lightning import Trainer, seed_everything
-from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint, RichModelSummary, RichProgressBar
-from pytorch_lightning.loggers import WandbLogger
-from torch.utils.data import DataLoader
+import random
+import string
 
-from src.data import DownstreamDataset
+import torch
+
+# import wandb
+from pytorch_lightning import Trainer, seed_everything
+from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint, RichProgressBar
+from pytorch_lightning.loggers import CSVLogger
+from torch.utils.data import DataLoader, random_split
+
+from src.data import DownstreamDataset, load_dataset
 from src.model import Model
 
 # technical setting to make sure, parallelization works if multiple models are trained in parallel
 torch.multiprocessing.set_sharing_strategy("file_system")
 
 
+def random_string(k: int = 5):
+    return "".join(random.choices(string.ascii_letters + string.digits, k=k))
+
+
 def train(
+    dataset: DownstreamDataset,
+    dataset_path: str,
     model_name: str,
     layer_num: int,
+    random_name: str,
     hidden_dim: int = 512,
     batch_size: int = 1024,
     max_epoch: int = 200,
     dropout: float = 0.2,
-    dataset: str = "/shared/stability",
     early_stopping_patience: int = 30,
     lr: float = 0.001,
     reduce_lr_patience: int = 30,
     seed: int = 42,
+    gpu: int = 0,
 ):
     # for reproducibility
     seed_everything(seed)
 
     # define the logger
-    logger = WandbLogger(
-        log_model=True,
-        project=dataset.split("/")[-1],
-        entity="smtb2024",
-        name=f"{model_name.split('_')[1]}_{layer_num:02d}",
-        config={
+    logger = CSVLogger(
+        save_dir="/scratch/logs",
+        name=f"{dataset_path.split('/')[-1][:-4]}_{model_name.split('_')[1]}_L{layer_num:02d}_{random_name}",
+    )
+
+    logger.log_hyperparams(
+        {
             "model_name": model_name,
             "layer_num": layer_num,
             "hidden_dim": hidden_dim,
@@ -42,20 +54,18 @@ def train(
             "early_stopping_patience": early_stopping_patience,
             "lr": lr,
             "reduce_lr_patience": reduce_lr_patience,
-        },
+        }
     )
-
     # define the callbacks with EarlyStopping and two more for nicer tracking
     callbacks = [
         EarlyStopping(monitor="val/loss", patience=early_stopping_patience, mode="min"),
-        RichModelSummary(),
         RichProgressBar(),
         ModelCheckpoint(monitor="val/loss", mode="min"),
     ]
 
     # define the Trainer and it's most important arguments
     trainer = Trainer(
-        devices=1,
+        devices=[gpu],
         max_epochs=max_epoch,
         callbacks=callbacks,
         logger=logger,
@@ -63,21 +73,12 @@ def train(
 
     # initialize the model
     model = Model(hidden_dim=hidden_dim, dropout=dropout)
+    print(model)
+    train, val, test = random_split(dataset, [0.7, 0.2, 0.1])
 
-    # look into the directory below
-    datasets = []
-    # TODO FIXME
-    # for ds in ["train", "validation", "test"]:
-    #     p = Path(dataset) / model_name / ds
-    #     datasets.append(train_validation_test(p, layer_num))
-
-    train_dataset = DownstreamDataset(datasets[0][0], datasets[0][1])
-    validation_dataset = DownstreamDataset(datasets[1][0], datasets[1][1])
-    test_dataset = DownstreamDataset(datasets[2][0], datasets[2][1])
-
-    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    validation_dataloader = DataLoader(validation_dataset, batch_size=batch_size)
-    test_dataloader = DataLoader(test_dataset, batch_size=batch_size)
+    train_dataloader = DataLoader(train, batch_size=batch_size, shuffle=True)
+    validation_dataloader = DataLoader(val, batch_size=batch_size)
+    test_dataloader = DataLoader(test, batch_size=batch_size)
 
     trainer.fit(model, train_dataloaders=train_dataloader, val_dataloaders=validation_dataloader)
 
@@ -97,7 +98,7 @@ model_names = {
 
 def run(
     num_layers: int,
-    dataset: str,
+    dataset_path: str,
     hidden_dim: int = 512,
     batch_size: int = 1024,
     max_epoch: int = 200,
@@ -106,6 +107,7 @@ def run(
     lr: float = 0.001,
     reduce_lr_patience: int = 30,
     seed: int = 42,
+    gpu: int = 0,
 ):
     """Runs the training of the model with the given parameters.
 
@@ -120,23 +122,31 @@ def run(
         lr (float, optional): Learning rate. Defaults to 0.001.
         reduce_lr_patience (int, optional): Patience for reducing the learning rate. Defaults to 50.
         seed (int, optional): Seed for reproducibility. Defaults to 42.
+        gpu (int, optional): GPU to use for computation
     """
+    random_name = random_string()
     model_name = model_names[num_layers]
-    for layer in range(num_layers + 1):
+    load_dataset(num_layers, dataset_path.split("/")[-1][:-4])
+    exit(0)
+    print(len(ds))
+    for i, dataset_path in enumerate(ds):
+        print("Train layer", i)
         train(
-            model_name,
-            layer,
-            hidden_dim,
-            batch_size,
-            max_epoch,
-            dropout,
-            dataset,
-            early_stopping_patience,
-            lr,
-            reduce_lr_patience,
-            seed,
+            dataset=dataset,
+            dataset_path=dataset_path,
+            model_name=model_name,
+            layer_num=i,
+            random_name=random_name,
+            hidden_dim=hidden_dim,
+            batch_size=batch_size,
+            max_epoch=max_epoch,
+            dropout=dropout,
+            early_stopping_patience=early_stopping_patience,
+            lr=lr,
+            reduce_lr_patience=reduce_lr_patience,
+            seed=seed,
+            gpu=gpu,
         )
-        wandb.finish()
 
 
 if __name__ == "__main__":
