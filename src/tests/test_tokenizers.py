a13a5919b2abde6bd58bb326044a5d4c1cf81121
Ilya Senatorov <il.senatorov@protonmail.com>
2024-08-16 13:05:35 +0200

    added pytest stuff

---
+++ src/tests/test_tokenizers.py
@@ -0,0 +1,38 @@
+from typing import get_args
+
+import pytest
+from datasets import Dataset, DatasetDict
+from transformers import PreTrainedTokenizerFast
+
+from ..tokenization import TOKENIZATION_TYPES, train_tokenizer
+
+
+def tokenization_types():
+    return get_args(TOKENIZATION_TYPES)
+
+
+@pytest.fixture
+def sample_dataset():
+    data = {
+        "train": {
+            "text": [
+                "SKGEELFTGVVPILVELDGDVNGHKFSVSGEGEGDATYGKLTLKFICTTGKLPVPWPTLVTTLSYGVQCFSRYPDHMKQHDFFKSAMPEGYVQERTIFFKDDGNYKTRDEVKFEGDTLVNRIELKGIDFKEDGNILGHKLEYNYDSHNVYIMADKQKNGIKVNFKIRHNIEDGSVQLADHYQQNTPVGDGPVLLPDNHYLSTQSALSKDPNEKRDHMVLLEFVTAAGI"
+            ],
+        }
+    }
+    return DatasetDict({"train": Dataset.from_dict(data)})
+
+
+@pytest.mark.parametrize("tokenization_type", tokenization_types())
+def test_train_tokenizer(tokenization_type, sample_dataset, tmp_path):
+    output_dir = tmp_path / "tokenization"
+    tokenizer = train_tokenizer(
+        dataset=sample_dataset, tokenization_type=tokenization_type, output_dir=output_dir, vocab_size=50
+    )
+    assert isinstance(tokenizer, PreTrainedTokenizerFast)
+    assert (output_dir / f"{tokenization_type}.json").exists()
+    assert tokenizer.pad_token == "[PAD]"
+    assert tokenizer.mask_token == "[MASK]"
+    assert tokenizer.cls_token == "[CLS]"
+    assert tokenizer.sep_token == "[SEP]"
+    assert tokenizer.unk_token == "[UNK]"
